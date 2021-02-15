9d466a0187f6288b8c8820a01e083fbd54419bcd
dipsy88 <dipsy.urfriend@gmail.com>
2021-02-15 19:50:03 +0100

    Added component skeleton

---
+++ src/test/java/com/rest/api/insurance/subject_system/SubjectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.insurance.subject_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
